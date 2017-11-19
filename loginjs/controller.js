9d4c573e0d7f4fdad6cdcfc76ebccd7eb35e4813
Shivani Taneja <shivani.taneja104@gmail.com>
2017-11-19 20:45:16 +0530

    Add files via upload

---
+++ loginjs/controller.js
@@ -0,0 +1,30 @@
+app.controller("myctrl",($scope, $window ,myfactory)=>{
+   
+    $scope.submitform = ()=>{
+    
+        var obj = {};
+        
+         
+        obj.username= "c"+ $scope.mobilenumber;
+        obj.password=$scope.password;
+        
+        console.log(obj)
+       
+          var promise = myfactory.callserver(obj);
+          promise.then((data)=>{
+                 
+                console.log(data);
+
+                if (data.key) {
+                    
+                     
+                    $window.location.href = "http://localhost/tools-master/login1.html";
+
+                }
+            },(err)=>{
+                
+                alert("error");
+            })
+       
+    }
+})
